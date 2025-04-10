25d25693d99c85bfdbcbf7ef4b87257e46eb41ac
LionLibel <rocketzgaming123@gmail.com>
2025-04-10 08:16:21 +0700

    Add files via upload

---
+++ application/app_config.py
@@ -0,0 +1,8 @@
+# Application configuration
+# This file will be updated by the OTA service
+
+# Data generation interval in seconds
+INTERVAL = 10
+
+# Additional configuration parameters can be added here
+# as the application evolves through OTA updates
