c1db83bec2283f7d4b2807096f63e4ed374fd95d
LionLibel <rocketzgaming123@gmail.com>
2025-04-13 13:10:56 +0700

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,16 @@
+# Enhanced Application configuration
+# This file will be updated by the OTA service
+
+# Data generation interval in seconds
+INTERVAL = 5
+
+# Data generation mode (random, sine, step, random_walk)
+DATA_MODE = "random_walk"
+
+# API settings (disabled for initial upgrade)
+API_ENABLED = False
+
+# Analysis settings
+ANALYSIS_ENABLED = True
+ANALYSIS_WINDOW = 60  # Window size in seconds
+ANALYSIS_INTERVAL = 30  # How often to run analysis in seconds
