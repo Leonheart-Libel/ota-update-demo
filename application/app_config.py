d7df09e4091422ec29ab917514a463ff1ce213c9
LionLibel <rocketzgaming123@gmail.com>
2025-04-15 21:12:39 +0700

    Add files via upload

--- application/app_config.py
+++ application/app_config.py
@@ -2,7 +2,7 @@
 # This file is updated by the OTA service
 
 # Data generation interval in seconds
-INTERVAL = 10
+INTERVAL = 5
 
 # Enable extended logging (more detailed output)
 ENABLE_EXTENDED_LOGGING = True
