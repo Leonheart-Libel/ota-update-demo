0e9927266d2ce649c4fde10734e94f4efffe9715
LionLibel <rocketzgaming123@gmail.com>
2025-04-23 22:51:32 +0700

    Add files via upload

--- application/app_config.py
+++ application/app_config.py
@@ -2,7 +2,7 @@
 # This file is updated by the OTA service
 
 # Data generation interval in seconds
-INTERVAL = 10
+INTERVAL = 15
 
 # Enable extended logging (more detailed output)
 ENABLE_EXTENDED_LOGGING = True
@@ -10,14 +10,12 @@ ENABLE_EXTENDED_LOGGING = True
 # Data retention period in days (0 = keep indefinitely)
 DATA_RETENTION_DAYS = 30
 
-# New simulation parameters
-ENABLE_UV_SIMULATION = True
-ENABLE_FEELS_LIKE = True
-MAX_WIND_GUST = 20  # km/h
-
 # Azure SQL Database connection parameters
 SQL_SERVER = "iotcentralhub.database.windows.net"
 SQL_DATABASE = "otasqldb"
 SQL_USERNAME = "rozemyne"
 SQL_PASSWORD = "alexandria_7"
 TRUST_SERVER_CERT = "no"
+
+# Additional configuration parameters can be added here
+# as the application evolves through OTA updates
